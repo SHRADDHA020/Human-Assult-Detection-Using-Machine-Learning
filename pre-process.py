f2eb5e093edee6dc13aedf93e06cbba91b94a08c
SHRADDHA S <68418144+SHRADDHA020@users.noreply.github.com>
2023-11-06 19:49:51 +0530

    Add files via upload

---
+++ pre-process.py
@@ -0,0 +1,150 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import os
+
+# Importing Deep Learning Libraries
+
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense,Input,Dropout,GlobalAveragePooling2D,Flatten,Conv2D,BatchNormalization,Activation,MaxPooling2D
+from keras.models import Model,Sequential
+from keras.optimizers import Adam,SGD,RMSprop
+
+picture_size = 48
+folder_path = "E:/UNZIP/Zzz/MONICA/images/images/"
+
+expression = 'disgust'
+
+plt.figure(figsize= (12,12))
+for i in range(1, 10, 1):
+    plt.subplot(3,3,i)
+    img = load_img(folder_path+"train/"+expression+"/"+
+                  os.listdir(folder_path + "train/" + expression)[i], target_size=(picture_size, picture_size))
+    plt.imshow(img)   
+plt.show()
+
+batch_size  = 128
+
+datagen_train  = ImageDataGenerator()
+datagen_val = ImageDataGenerator()
+
+train_set = datagen_train.flow_from_directory(folder_path+"train",
+                                              target_size = (picture_size,picture_size),
+                                              color_mode = "grayscale",
+                                              batch_size=batch_size,
+                                              class_mode='categorical',
+                                              shuffle=True)
+
+
+test_set = datagen_val.flow_from_directory(folder_path+"validation",
+                                              target_size = (picture_size,picture_size),
+                                              color_mode = "grayscale",
+                                              batch_size=batch_size,
+                                              class_mode='categorical',
+                                              shuffle=False)
+
+no_of_classes = 7
+
+model = Sequential()
+
+#1st CNN layer
+model.add(Conv2D(64,(3,3),padding = 'same',input_shape = (48,48,1)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout(0.25))
+
+#2nd CNN layer
+model.add(Conv2D(128,(5,5),padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout (0.25))
+
+#3rd CNN layer
+model.add(Conv2D(512,(3,3),padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout (0.25))
+
+#4th CNN layer
+model.add(Conv2D(512,(3,3), padding='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+
+#Fully connected 1st layer
+model.add(Dense(256))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+
+# Fully connected layer 2nd layer
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(no_of_classes, activation='softmax'))
+
+
+
+opt = Adam(lr = 0.0001)
+model.compile(optimizer=opt,loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+from keras.optimizers import RMSprop,SGD,Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+checkpoint = ModelCheckpoint("./model.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+early_stopping = EarlyStopping(monitor='val_loss',
+                          min_delta=0,
+                          patience=3,
+                          verbose=1,
+                          restore_best_weights=True
+                          )
+
+reduce_learningrate = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              patience=3,
+                              verbose=1,
+                              min_delta=0.0001)
+
+callbacks_list = [early_stopping,checkpoint,reduce_learningrate]
+
+epochs = 48
+
+model.compile(loss='categorical_crossentropy',
+              optimizer = Adam(lr=0.001),
+              metrics=['accuracy'])
+history = model.fit_generator(generator=train_set,
+                                steps_per_epoch=train_set.n//train_set.batch_size,
+                                epochs=epochs,
+                                validation_data = test_set,
+                                validation_steps = test_set.n//test_set.batch_size,
+                                callbacks=callbacks_list
+                                )
+plt.style.use('dark_background')
+
+plt.figure(figsize=(20,10))
+plt.subplot(1, 2, 1)
+plt.suptitle('Optimizer : Adam', fontsize=10)
+plt.ylabel('Loss', fontsize=16)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend(loc='upper right')
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy', fontsize=16)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.show()
